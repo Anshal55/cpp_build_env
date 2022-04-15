06c1155b186ba065808bfed50b0d57ebb772abf2
Anshal Singh <Anshal.54321@gmail.com>
2022-04-15 18:10:40 +0530

    added user input

--- src/app/fibonacci/fib.cc
+++ src/app/fibonacci/fib.cc
@@ -4,11 +4,15 @@
 
 int main()
 {
+    int num;
     std::cout << "\n============================================\n";
+    std::cout << "WARNING:: For larger numbers(>49) unoptimized solution will take quite long.\n";
+    std::cout << "Enter the number :: ";
+    std::cin >> num;
     {
         // Get unoptimized fibonacci with timer
         Timer t;
-        auto ans = Fib::unopt_fib(43);
+        auto ans = Fib::unopt_fib(num);
         std::cout << "\nUnoptimized Fib:: " << ans << "\n";
         std::cout << "Unoptimized:: ";
     }
@@ -16,7 +20,7 @@ int main()
     {
         // Get optimized fibonacci with timer
         Timer t;
-        auto ans = Fib::opt_fib(43);
+        auto ans = Fib::opt_fib(num);
         std::cout << "\nOptimized Fib:: " << ans << "\n";
         std::cout << "Optimized:: ";
     }
