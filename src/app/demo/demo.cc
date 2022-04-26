acab3ba58591ea052744abb98aebdea0c8f6306c
Anshal Singh <anshal.singh@myelinfoundry.com>
2022-04-26 16:13:57 +0530

    more changes and dependencies

---
+++ src/app/demo/demo.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// GLOG
+#include "glog/logging.h"
+#include "glog/stl_logging.h"
+
+// absl
+#include "absl/strings/str_join.h"
+
+// Internal Dependencies
+#include "src/includes/timer/timer.h"
+
+int main(int argc, const char **argv)
+{
+    google::InitGoogleLogging(argv[0]);
+
+    std::vector<std::string> vec = {"foo", "bar", "baz"};
+    absl::string_view s = absl::StrJoin(vec, "-");
+
+    LOG(INFO) << "Str:: " << s << "\n";
+    LOG_IF(ERROR, s.size() > 10) << "Something went wrong.\n";
+
+    LOG_IF(FATAL, !std::stoi(argv[1])) << "ERROR:: FIX THIS.";
+
+    return 0;
+}
--- src/app/fibonacci/fib.cc
+++ src/app/fibonacci/fib.cc
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+
 // GLOG
 #include "glog/logging.h"
 #include "glog/stl_logging.h"
+
+// External Dependencies
+#include "src/lib/cpplib.h"
 
 // Internal Dependencies
 #include "src/includes/fibonacci/fiblib.h"
@@ -12,10 +17,13 @@ int main(int argc, const char **argv)
     google::InitGoogleLogging(argv[0]);
 
     int num;
-    std::cout << "\n========================================================================================:)\n";
-    LOG(WARNING) << "For larger numbers(>45) unoptimized solution will take quite long.\n";
+    std::cout << "\n========================================================================================:\n";
+    LOG(INFO) << "For larger numbers(>45) unoptimized solution will take quite long.\n";
     std::cout << "Enter the number :: ";
     std::cin >> num;
+
+    LOG_IF(FATAL, num > 45) << "Too large a value, will take a lot of time.\n";
+
     {
         // Get unoptimized fibonacci with timer
         Timer t;
@@ -31,7 +39,18 @@ int main(int argc, const char **argv)
         std::cout << "\nOptimized Fib:: " << ans << "\n";
         std::cout << "Optimized:: ";
     }
-    std::cout << "\n========================================================================================:)\n";
+    std::cout << "\n========================================================================================:\n";
+
+    // Log stl containers
+    // std::vector<std::string> vec{"foo", "bar", "test"};
+    // LOG(INFO) << "VEC: {" << vec << "}\n";
+
+    // External dependency test
+    // CPPLib test;
+    // LOG(INFO) << test.PrintHelloWorld();
+
+    // VLOG(1) << "testing this thing maybe.";
+    // CHECK_EQ("anshal", "ansha");
 
     return 0;
 }
